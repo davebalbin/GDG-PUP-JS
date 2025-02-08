7d49decb990fed31cfe04a529c640050f96a3aad
Dave <dave.balbin25@gmail.com>
2025-02-09 00:05:10 +0800

    Add files via upload

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/JS5.js
@@ -0,0 +1,19 @@
+// Create an array named favoriteMovies
+const favoriteMovies = [
+    "Wicked",
+    "Jimmy Neutron: Boy Genius",
+    "Cruella",
+    "Harry Potter",
+    "Alice in Borderland",
+    "Sweet Home",
+    "Squid Game",
+    "20th Century Girl",
+    "The Lord of the Rings: The Return of the King",
+    "The Hunger Games"
+];
+
+// Use a loop to iterate through the favoriteMovies array
+console.log("Favorite Movies:");
+for (let i = 0; i < favoriteMovies.length; i++) {
+    console.log(favoriteMovies[i]); // Log each movie to the console
+}
